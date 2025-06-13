ebe5c3c05844fbd791e1caab3a32fabe6fdee017
Ndolo Mercy <mercymwendend444@gmail.com>
2025-06-13 03:20:55 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+//code that gives 62
+let num1 = 31;
+let num2 = 2;
+let multiply =(num1* num2);
+console.log(multiply);
+//"generates a random integer greater than 0
+let random = Math.floor(Math.random()*10)
+console.log(random);
+///////generates a remainder that equal to 4
+let num3=14;
+let num4=5
+let mod = num3 % num4;
+console.log (mod);
+/////////
+let max= Math.max(19,7,20,3);
+console.log (max);
